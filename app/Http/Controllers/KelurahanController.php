552f9e320b946192d865ad0e176af1139af9e851
IsnainPermana <isnainpermana@gmail.com>
2026-06-22 18:03:44 +0700

    FINAL RELEASE: Menyelesaikan Modul 12 - Upload Berkas, Cetak PDF, dan Finalisasi Simpel-K

--- app/Http/Controllers/KelurahanController.php
+++ app/Http/Controllers/KelurahanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Surat;
 
-class SuratController extends Controller
+class KelurahanController extends Controller
 {
     public function daftarSurat() {
     // Eager Loading relasi penduduk untuk menghemat query
